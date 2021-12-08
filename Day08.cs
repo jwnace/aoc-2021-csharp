cbf55cd3b54c064da443ec47c05e2cc72ffd5626
Joe Nace <jwnace@gmail.com>
2021-12-08 02:08:39 -0500

    cleaned up solution for day 08

--- Day08.cs
+++ Day08.cs
@@ -17,7 +17,7 @@ namespace aoc_2021_csharp
             {
                 var values = line.Split(" | ");
                 var digits = values[1].Split(' ');
-                sum += digits.Count(x => new List<int> { 2, 3, 4, 7 }.Any(y => y == x.Length));
+                sum += digits.Count(x => new List<int> { 2, 3, 4, 7 }.Contains(x.Length));
             }
 
             Console.WriteLine($"Day 08, Part 1: {sum}");
@@ -30,97 +30,50 @@ namespace aoc_2021_csharp
             foreach (var line in input)
             {
                 var values = line.Split(" | ");
-                var patterns = values[0].Split(' ');
-                var digits = values[1].Split(' ');
-                var stringToInt = new Dictionary<string, int>();
-                var intToString = new Dictionary<int, string>();
+                var patterns = values[0].Split(' ').Select(x => x.Sort());
+                var digits = values[1].Split(' ').Select(x => x.Sort());
+                var dict = new Dictionary<string, int>();
 
-                // 1
-                var t1 = new string(patterns.Single(x => x.Length == 2).OrderBy(c => c).ToArray());
-                stringToInt[t1] = 1;
-                intToString[1] = t1;
-
-                // 7
-                var t2 = new string(patterns.Single(x => x.Length == 3).OrderBy(c => c).ToArray());
-                stringToInt[t2] = 7;
-                intToString[7] = t2;
-
-                // 4
-                var t3 = new string(patterns.Single(x => x.Length == 4).OrderBy(c => c).ToArray());
-                stringToInt[t3] = 4;
-                intToString[4] = t3;
-
-                // 8
-                var t4 = new string(patterns.Single(x => x.Length == 7).OrderBy(c => c).ToArray());
-                stringToInt[t4] = 8;
-                intToString[8] = t4;
-
-                foreach (var pattern in patterns)
-                {
-                    var temp = new string(pattern.OrderBy(c => c).ToArray());
-
-                    if (stringToInt.ContainsKey(temp))
-                    {
-                        continue;
-                    }
-
-                    if (temp.Length == 5)
-                    {
-                        if (temp.Intersect(intToString[1]).Count() == 2)
-                        {
-                            // 3
-                            stringToInt[temp] = 3;
-                            intToString[3] = temp;
-                        }
-                        else if (temp.Intersect(intToString[4]).Count() == 2)
-                        {
-                            // 2
-                            stringToInt[temp] = 2;
-                            intToString[2] = temp;
-                        }
-                        else
-                        {
-                            // 5
-                            stringToInt[temp] = 5;
-                            intToString[5] = temp;
-                        }
-                    }
-                    else if (temp.Length == 6)
-                    {
-                        if (temp.Intersect(intToString[1]).Count() != 2)
-                        {
-                            // 6
-                            stringToInt[temp] = 6;
-                            intToString[6] = temp;
-                        }
-                        else if (temp.Intersect(intToString[4]).Count() == 4)
-                        {
-                            // 9
-                            stringToInt[temp] = 9;
-                            intToString[9] = temp;
-                        }
-                        else
-                        {
-                            // 0
-                            stringToInt[temp] = 0;
-                            intToString[0] = temp;
-                        }
-                    }
-                }
+                var one = patterns.Single(x => x.Length == 2);
+                dict[one] = 1;
+                var seven = patterns.Single(x => x.Length == 3);
+                dict[seven] = 7;
+                var four = patterns.Single(x => x.Length == 4);
+                dict[four] = 4;
+                var eight = patterns.Single(x => x.Length == 7);
+                dict[eight] = 8;
+                var three = patterns.Single(x => x.Length == 5 && x.Intersect(one).Count() == 2);
+                dict[three] = 3;
+                var two = patterns.Single(x => x.Length == 5 && x.Intersect(four).Count() == 2);
+                dict[two] = 2;
+                var five = patterns.Single(x => x.Length == 5 && !dict.ContainsKey(x));
+                dict[five] = 5;
+                var six = patterns.Single(x => x.Length == 6 && x.Intersect(one).Count() != 2);
+                dict[six] = 6;
+                var nine = patterns.Single(x => x.Length == 6 && x.Intersect(four).Count() == 4);
+                dict[nine] = 9;
+                var zero = patterns.Single(x => !dict.ContainsKey(x));
+                dict[zero] = 0;
 
                 var outputString = "";
 
                 foreach (var digit in digits)
                 {
-                    var temp = new string(digit.OrderBy(c => c).ToArray());
-
-                    outputString += stringToInt[temp];
+                    outputString += dict[digit];
                 }
 
                 sum += int.Parse(outputString);
             }
 
             Console.WriteLine($"Day 08, Part 2: {sum}");
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static string Sort(this string str)
+        {
+            return new string(str.OrderBy(c => c).ToArray());
         }
     }
 }
