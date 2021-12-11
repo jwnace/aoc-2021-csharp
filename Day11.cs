6cc5b6ed1318daa8300b343bd4e80fabb0d2fc54
Joe Nace <jwnace@gmail.com>
2021-12-11 12:02:07 -0500

    cleaned up solution for day 11

--- Day11.cs
+++ Day11.cs
@@ -8,19 +8,26 @@ namespace aoc_2021_csharp
     {
         private static readonly string INPUT_FILE = "input/day11.txt";
         private static readonly string[] input = System.IO.File.ReadAllLines(INPUT_FILE);
+        private readonly List<List<Octopus>> grid = input.Select(x => x.Select(y => new Octopus(y.ToInt())).ToList()).ToList();
 
         public void Part1()
         {
-            var grid = input.Select(x => x.Select(y => new Octopus(y.ToInt())).ToList()).ToList();
-            var steps = 100;
-            var countFlashes = 0;
+            Run(1);
+        }
 
-            for (int step = 0; step < steps; step++)
+        public void Part2()
+        {
+            Run(2);
+        }
+
+        private void Run(int part)
+        {
+            var count = 0;
+
+            for (int step = 1; step < int.MaxValue; step++)
             {
                 // increase the energy level of every octopus
                 grid.SelectMany(x => x).ToList().ForEach(x => x.EnergyLevel++);
-
-                var flashes = new List<(int, int)>();
 
                 // process all the flashes for the current step (keep going until all the energy levels are <= 9)
                 while (grid.SelectMany(x => x).Any(x => x.EnergyLevel > 9))
@@ -31,107 +38,47 @@ namespace aoc_2021_csharp
                         {
                             if (grid[i][j].EnergyLevel > 9)
                             {
-                                // increment all neighbors by 1
-                                var neighbors = new List<(int, int)>
-                                {
-                                    (i - 1, j - 1), (i - 1, j), (i - 1, j + 1),
-                                    (i    , j - 1),             (i    , j + 1),
-                                    (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
-                                };
-
-                                neighbors.ForEach(x =>
-                                {
-                                    var neighbor = grid.ElementAtOrDefault(x.Item1)?.ElementAtOrDefault(x.Item2);
-
-                                    if (neighbor != null)
-                                    {
-                                        neighbor.EnergyLevel++;
-                                    }
-                                });
-
-                                // minor hack to prevent an octopus from flashing more than once per step
-                                grid[i][j].EnergyLevel = int.MinValue;
-
-                                // keep track of each location that flashed
-                                flashes.Add((i, j));
+                                Flash(i, j);
+                                count++;
                             }
                         }
                     }
                 }
 
-                for (int i = 0; i < flashes.Count(); i++)
+                if (part == 1 && step == 100)
                 {
-                    // set the energy level to 0 for all locations that flashed
-                    grid[flashes[i].Item1][flashes[i].Item2].EnergyLevel = 0;
+                    Console.WriteLine($"Day 11, Part 1: {count}");
+                    return;
                 }
-
-                countFlashes += flashes.Count();
-            }
-
-            Console.WriteLine($"Day 11, Part 1: {countFlashes}");
-        }
-
-        public void Part2()
-        {
-            var grid = input.Select(x => x.Select(y => new Octopus(y.ToInt())).ToList()).ToList();
-            var step = 0;
-
-            while (true)
-            {
-                step++;
-
-                // increase the energy level of every octopus
-                grid.SelectMany(x => x).ToList().ForEach(x => x.EnergyLevel++);
-
-                var flashes = new List<(int, int)>();
-
-                while (grid.SelectMany(x => x).Any(x => x.EnergyLevel > 9))
-                {
-                    for (int i = 0; i < grid.Count(); i++)
-                    {
-                        for (int j = 0; j < grid[i].Count(); j++)
-                        {
-                            if (grid[i][j].EnergyLevel > 9)
-                            {
-                                // increment all neighbors by 1
-                                var neighbors = new List<(int, int)>
-                                {
-                                    (i - 1, j - 1), (i - 1, j), (i - 1, j + 1),
-                                    (i    , j - 1),             (i    , j + 1),
-                                    (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
-                                };
-
-                                neighbors.ForEach(x =>
-                                {
-                                    var neighbor = grid.ElementAtOrDefault(x.Item1)?.ElementAtOrDefault(x.Item2);
-
-                                    if (neighbor != null)
-                                    {
-                                        neighbor.EnergyLevel++;
-                                    }
-                                });
-
-                                // minor hack to prevent an octopus from flashing more than once per step
-                                grid[i][j].EnergyLevel = int.MinValue;
-
-                                // keep track of each location that flashed
-                                flashes.Add((i, j));
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < flashes.Count(); i++)
-                {
-                    grid[flashes[i].Item1][flashes[i].Item2].EnergyLevel = 0;
-                }
-
-                if (grid.All(r => r.All(c => c.EnergyLevel == 0)))
+                else if (part == 2 && grid.All(r => r.All(c => c.EnergyLevel == 0)))
                 {
                     Console.WriteLine($"Day 11, Part 2: {step}");
                     return;
                 }
             }
+        }
+
+        private void Flash(int i, int j)
+        {
+            // increment all neighbors by 1
+            var neighbors = new List<(int, int)>
+            {
+                (i - 1, j - 1), (i - 1, j), (i - 1, j + 1),
+                (i    , j - 1),             (i    , j + 1),
+                (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
+            };
+
+            neighbors.ForEach(x =>
+            {
+                var neighbor = grid.ElementAtOrDefault(x.Item1)?.ElementAtOrDefault(x.Item2);
+
+                if (neighbor != null && neighbor.EnergyLevel != 0)
+                {
+                    neighbor.EnergyLevel++;
+                }
+            });
+
+            grid[i][j].EnergyLevel = 0;
         }
     }
 
