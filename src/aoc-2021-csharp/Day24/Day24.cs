5c69c37b8042abcb3a1b5be7168d6a10e8f2f7cd
Joe Nace <jwnace@gmail.com>
2023-10-24 11:13:42 -0400

    updated boilerplate for day 24

--- src/aoc-2021-csharp/Day24/Day24.cs
+++ src/aoc-2021-csharp/Day24/Day24.cs
@@ -2,9 +2,19 @@ namespace aoc_2021_csharp.Day24;
 
 public static class Day24
 {
-    private static readonly string[] Input = File.ReadAllLines("Day24/day24.txt");
+    private static readonly string Input = File.ReadAllText("Day24/day24.txt").Trim();
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    private static int Solve1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static int Solve2(string input)
+    {
+        throw new NotImplementedException();
+    }
 }
