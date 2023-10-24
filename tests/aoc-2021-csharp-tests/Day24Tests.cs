8b29a933f07ff642beea3c38e8f85acabb70bca1
Joe Nace <jwnace@gmail.com>
2023-10-23 23:43:32 -0400

    added tests for day 24

---
+++ tests/aoc-2021-csharp-tests/Day24Tests.cs
@@ -0,0 +1,18 @@
+using aoc_2021_csharp.Day24;
+
+namespace aoc_2021_csharp_tests;
+
+public class Day24Tests
+{
+    [Test]
+    public void Part1_ReturnsCorrectAnswer()
+    {
+        Day24.Part1().Should().Be(0);
+    }
+
+    [Test]
+    public void Part2_ReturnsCorrectAnswer()
+    {
+        Day24.Part2().Should().Be(0);
+    }
+}
