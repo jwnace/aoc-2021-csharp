a932fa4af7732f7e54849855371b4e7664c1cf46
Joe Nace <jwnace@gmail.com>
2023-10-25 11:36:25 -0400

    added tests for examples

--- src/aoc-2021-csharp/Day23/Day23.cs
+++ src/aoc-2021-csharp/Day23/Day23.cs
@@ -2,18 +2,18 @@ namespace aoc_2021_csharp.Day23;
 
 public static class Day23
 {
-    private static readonly string Input = File.ReadAllText("Day23/day23.txt").Trim();
+    private static readonly string[] Input = File.ReadAllLines("Day23/day23.txt");
 
     public static int Part1() => Solve1(Input);
 
     public static int Part2() => Solve2(Input);
 
-    private static int Solve1(string input)
+    public static int Solve1(string[] input)
     {
         throw new NotImplementedException();
     }
 
-    private static int Solve2(string input)
+    public static int Solve2(string[] input)
     {
         throw new NotImplementedException();
     }
--- tests/aoc-2021-csharp-tests/Day23Tests.cs
+++ tests/aoc-2021-csharp-tests/Day23Tests.cs
@@ -5,9 +5,41 @@ namespace aoc_2021_csharp_tests;
 public class Day23Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "#############",
+            "#...........#",
+            "###B#C#B#D###",
+            "  #A#D#C#A#",
+            "  #########",
+        };
+
+        Day23.Solve1(input).Should().Be(12521);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         Day23.Part1().Should().Be(18195);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "#############",
+            "#...........#",
+            "###B#C#B#D###",
+            "  #D#C#B#A#",
+            "  #D#B#A#C#",
+            "  #A#D#C#A#",
+            "  #########",
+        };
+
+        Day23.Solve2(input).Should().Be(44169);
     }
 
     [Test]
